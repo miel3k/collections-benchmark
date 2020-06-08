b96b0f5b934bf451ce5e9487a43bea4a150016b9
miel3k <kuba.mielczarek96@icloud.com>
2020-06-08 21:59:04 +0200

    Create App class

--- src/main/java/com/miel3k/collectionsbenchmark/App.java
+++ src/main/java/com/miel3k/collectionsbenchmark/App.java
@@ -1,4 +1,16 @@
 package com.miel3k.collectionsbenchmark;
 
-public class App {
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "jbm", mixinStandardHelpOptions = true, version = "jbm - Java Collections Benchmark 0.0.1")
+public class App implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from Java Collections Benchmark");
+    }
+
+    public static void main(final String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
 }
